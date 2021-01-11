21bb49561b22f4fee47d940168cb1269593194f7
akiragondo <safegondo@gmail.com>
2021-01-11 18:31:40 -0300

    Add detector and colors

--- detectors/mainDetector.py
+++ detectors/mainDetector.py
@@ -4,21 +4,48 @@ from .detectorInterface import DetectorInterface
 
 class MainDetector(DetectorInterface):
     def __init__(self):
-        self.lastResistances = np.array([])
+        """
+        Idea behind detector: if it detects a deviation between max - min bigger than a percentage of the moving
+        average, it gets into state 1 and puts boundaries a percentage amount <100% of the threshold around the
+        moving average. If the resistance keeps itself outside of these boundaries for a number of minutes,
+        it will be  detected as a contamination
+        """
+        self.previousResistances = np.array([])
+        self.previousFrequencies = np.array([])
 
-        self.detectionTime = None
-        self.currentResistance = None
+        self.initialOnsetTime = 10 #minutes
+        self.hysteresisWaitTime = 5 #minutes
 
-        #Variable thresholds for detections
-        #initial derivative for starting detection
-        self.initialDiffDetectionThresh = 0.05
-
-        #percentage of difference in resistance to detect a contamination
-        self.resistanceDiffDetectionThresh = 2
+        self.hysteresisPercentage = 30 #min to top hysteresis for detection stability
+        self.initialThreshold = 2 #threshold for initial detection
+        """
+            code:
+                0 - Undetected
+                1 - initial detected
+                2 - anomaly detected
+        """
+        self.state = 0
+        self.hysteresisResTop = None
+        self.hysteresisResBottom = None
 
     def detectAnomaly(self, sample: np.ndarray) -> int:
-        if self.lastResistance is None:
-            self.lastResistance = sample[0][2]
-            self.currentResistance = sample[0][2]
+        if len(self.previousResistances < 15):
+            self.previousResistances = np.append(self.previousResistances, np.mean(sample[:,2]))
+            self.previousFrequencies = np.append(self.previousFrequencies, np.mean(sample[:,1]))
+            return -1
+        else:
+            self.previousResistances = np.roll(self.previousResistances,-1)
+            self.previousResistances[-1] = np.mean(sample[:,2])
+            self.previousFrequencies = np.roll(self.previousFrequencies,-1)
+            self.previousFrequencies[-1] = np.mean(sample[:,1])
+
+            averageResistance = np.mean(self.previousResistances)
+
+            if self.state == 0:
+                deviation = np.max(self.previousResistances) - np.mean(self.previousResistances)
+                deviation = (deviation/averageResistance)*100
+                if deviation > self.initialThreshold:
+                    self.state = 1
+                    self.hysteresisResTop = self.averageResistance
 
 
--- interface/mainApp.py
+++ interface/mainApp.py
@@ -1,33 +1,47 @@
 from .mainWindow import Ui_MainWindow
-from comms.rs232 import list_serial_ports,RS232
+from comms.rs232 import list_serial_ports, RS232
 import pyqtgraph as pg
-from PyQt5 import QtCore,QtWidgets
-from utils.utils import DateAxisItem
+from PyQt5 import QtCore, QtWidgets
 import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 from detectors.stability import isStable
+from datetime import datetime
+
+
+
 
 class MainApp(Ui_MainWindow):
-    def __init__(self,window):
-        #UI Setup
+    def __init__(self, window):
+        # UI Setup
         self.setupUi(window)
         self.window = window
         self.state = 0
         self.print = False
+        """
+            Code: 
+                red/orange -> Contaminations serious/mild
+                blue -> system operations (connection, disconnection, detection begins)
+                white -> system errors (timeouts)
+        """
+        self.colors = {
+            "red" : "#B80000",
+            "blue" : "#027CA1",
+            "white" : "#F3F6F5",
+            "orange" : "#FF8811"
+        }
 
-
-        #Time configurations
+        # Time configurations
         self.stabilizationTime = 300
-        self.visibleTime = 60*30
+        self.visibleTime = 60 * 30
         self.drawPeriod = 5
         self.detectPeriod = 60
         self.savePeriod = 30
 
-        #update viewbox
+        # update viewbox
         self.updateViews()
         self.plotLine.getViewBox().sigResized.connect(self.updateViews)
 
-        #Interactions - Connections
+        # Interactions - Connections
         self.is_simulated = False
         self.refresh_ports_combo()
         self.refreshButton.clicked.connect(self.refresh_ports_combo)
@@ -36,14 +50,13 @@ class MainApp(Ui_MainWindow):
         self.connectionTypeComboBox.currentTextChanged.connect(self.toggleSimulatorPort)
         self.searchButton.clicked.connect(self.searchOutputDirectory)
 
-        #QCM Setup
+        # QCM Setup
         self.rs = RS232()
         self.index = 0
 
         self.timer = QtCore.QTimer()
         self.timer.setInterval(1000)
         self.timer.timeout.connect(self.updatePlot)
-
 
         # date_axis = pg.graphicsItems.DateAxisItem.DateAxisItem(orientation = 'bottom')
         # self.graphWidget = PlotWidget(self.verticalLayoutWidget,
@@ -72,7 +85,24 @@ class MainApp(Ui_MainWindow):
         #
         # self.twinLine = pg.PlotCurveItem(pen = pg.mkPen(color='#FF8811',width=3))
         # self.twinGraph.addItem(self.twinLine)
+        self.textBrowser.clear()
 
+    def addPastEvent(self, time, description, color):
+        """
+        :param time: time in timestamp from the results
+        """
+        timeText = datetime.fromtimestamp(time).strftime("%H:%M - %d/%m/%Y")
+        content = "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; " \
+                  "-qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:\'arial\',\'sans-serif\'; " \
+                  "font-size:14px; color:{};\">•</span><span style=\"font-family:\'arial\',\'sans-serif\'; " \
+                  "font-size:14px; color:#f3f6f5;\"> {} - {}</span></p></td></tr></table>\n".format(color,
+                                                                                                    timeText,
+                                                      description)
+        content = content + self.textBrowser.toHtml()
+        self.textBrowser.setText(content)
+
+    def currentTime(self):
+        return(self.rs.datetime_to_float(datetime.now()))
 
     def searchOutputDirectory(self):
         file = str(QtWidgets.QFileDialog.getExistingDirectory(self.window, "Select Data Output Directory"))
@@ -81,7 +111,7 @@ class MainApp(Ui_MainWindow):
     def toggleSimulatorPort(self):
         if self.connectionTypeComboBox.currentText() == 'Simulation File':
             self.is_simulated = True
-            self.timer.setInterval(1000/60)
+            self.timer.setInterval(1000 / 60)
             self.refreshButton.setText('Search')
             self.refreshButton.clicked.disconnect()
             self.refreshButton.clicked.connect(self.searchSimulatorFile)
@@ -110,32 +140,32 @@ class MainApp(Ui_MainWindow):
         data = self.rs.read_data()
         if data != None:
             if self.print:
-                print ('Time: {} - Res: {} - Freq - {}'.format(data['Time'], data['Resistance'], data['Frequency']))
+                print('Time: {} - Res: {} - Freq - {}'.format(data['Time'], data['Resistance'], data['Frequency']))
             self.index += 1
 
-        if self.index % self.drawPeriod == 0:
-            self.plotLine.setData(self.rs.results[:,0], self.rs.results[:,2])
-            self.twinLine.setData(self.rs.results[:,0], self.rs.results[:,1])
-            #Update X Range
-            if (self.rs.results[:,0][-1]- self.rs.results[:,0][0] > self.visibleTime):
-                self.plotLine.getViewBox().setXRange(self.rs.results[:,0][-1] - self.visibleTime, self.rs.results[:,0][-1])
-
-        if self.index >= self.stabilizationTime and self.state == 1 and self.index % self.stabilizationTime/2 == 0:
-            sample = self.rs.results[:,2][-self.stabilizationTime:]
-            if isStable(sample,self.stabilizationTime):
+        if self.index >= self.stabilizationTime and self.state == 1 and self.index % self.stabilizationTime / 2 == 0:
+            sample = self.rs.results[:, 2][-self.stabilizationTime:]
+            if isStable(sample, self.stabilizationTime):
                 self.state = 2
+                self.addPastEvent(self.rs.results[:,0][-1], 'System stabilized', self.colors['blue'])
                 self.resultsLabel.setText('Ready')
                 self.progressBar.setValue(100)
-                self.add_vline(self.rs.results[:,0][-1])
+                self.add_vline(self.rs.results[:, 0][-1])
 
-
-        if self.index %self.savePeriod == 0:
+        if self.index % self.savePeriod == 0:
             self.rs.append_to_csv()
 
 
+        if self.index % self.drawPeriod == 0:
+            self.plotLine.setData(self.rs.results[:, 0], self.rs.results[:, 2])
+            self.twinLine.setData(self.rs.results[:, 0], self.rs.results[:, 1])
+            # Update X Range
+            if (self.rs.results[:, 0][-1] - self.rs.results[:, 0][0] > self.visibleTime):
+                self.plotLine.getViewBox().setXRange(self.rs.results[:, 0][-1] - self.visibleTime,
+                                                     self.rs.results[:, 0][-1])
+
     def toggle_connect(self):
         self.is_connected = not self.is_connected
-
 
     def refresh_ports_combo(self):
         ports_list = list_serial_ports()
@@ -151,7 +181,7 @@ class MainApp(Ui_MainWindow):
 
             self.rc = RS232()
 
-            #Disconnect
+            # Disconnect
             self.state = 0
             self.progressBar.setValue(0)
             self.resultsLabel.setText('')
@@ -163,14 +193,14 @@ class MainApp(Ui_MainWindow):
             self.refreshButton.setEnabled(True)
             self.fileName.setEnabled(True)
         else:
-            #Connect
+            # Connect
             port = self.portComboBox.currentText()
             gate_time = 1000
             scale_factor = 200
             output_folder = self.outputField.text()
             if output_folder[-1] != '/':
                 output_folder += '/'
-            self.rs.set_output_file(output_folder,self.fileName.text())
+            self.rs.set_output_file(output_folder, self.fileName.text())
             result = self.rs.establish_connection(
                 port_name=port,
                 gate_time=gate_time,
@@ -181,14 +211,15 @@ class MainApp(Ui_MainWindow):
             if result == 0:
                 self.toggle_connect()
                 self.state = 1
-                #Connected - Waiting Stabilization
-                #Stabilised - Training Model
-                #Ready
+                self.addPastEvent(self.currentTime(), 'Connection started', self.colors['blue'])
+                # Connected - Waiting Stabilization
+                # Stabilised - Training Model
+                # Ready
                 self.progressBar.setValue(20)
                 self.resultsLabel.setText('Connected - Waiting Stabilization')
 
                 self.connectButton.setText('Cancel')
-                #Disable all buttons
+                # Disable all buttons
                 self.portComboBox.setEnabled(False)
                 self.checkBox.setEnabled(False)
                 self.emailField.setEnabled(False)
@@ -196,6 +227,3 @@ class MainApp(Ui_MainWindow):
                 self.refreshButton.setEnabled(False)
                 self.fileName.setEnabled(False)
                 self.timer.start()
-
-
-
