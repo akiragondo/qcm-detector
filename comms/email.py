c2773a345aeb63e89d23eca23ee205e6ffc23f09
Akira Gondo <safegondo@gmail.com>
2021-01-12 15:23:16 -0300

    Add interface to aws and detection json body

--- comms/email.py
+++ comms/email.py
@@ -1,15 +1,30 @@
 import requests
+import json
 
 class EmailComm():
     def __init__(self):
         self.postEndpoint = "https://0mm3hlaop6.execute-api.us-east-2.amazonaws.com/QCMTest/emailSender"
+        self.verifyEndpoint = "https://0mm3hlaop6.execute-api.us-east-2.amazonaws.com/QCMTest/emailVerify"
         self.buffer = []
 
     def sendEmail(self, body):
         try:
             response = requests.post(self.postEndpoint, data = body)
+            return response
         except requests.exceptions.HTTPError as error:
             print(error)
             self.buffer.append(body)
 
+    def verifyEmail(self, email):
+        jsonData = json.dumps(
+            {
+                'to' : email
+            }
+        )
+        try:
+            response = requests.post(self.verifyEndpoint, data = jsonData)
+            return response
+        except requests.exceptions.HTTPError as error:
+            print(error)
+            return error
 
